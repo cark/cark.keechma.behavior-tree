c7cb64de3187dfb8d89b540fe27b1378a7bfdfa7
cark <carkhy@gmail.com>
2019-07-16 06:44:23 +0200

    added checkout and copy to production.bat

--- production.bat
+++ production.bat
@@ -4,3 +4,6 @@ md gh-pages\js
 call shadow-cljs release example
 copy resources\public\example\index.html gh-pages
 copy resources\public\example\js\main.js gh-pages\js
+git checkout gh-pages
+copy /Y gh-pages\index.html .
+copy /Y ghpages\js\main.js .\js
