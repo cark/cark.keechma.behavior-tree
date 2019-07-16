34182a93bcf9302c420fba9c7d1dd34569e8f071
cark <carkhy@gmail.com>
2019-07-16 07:05:23 +0200

    removed production

--- production.bat
+++
@@ -1,9 +0,0 @@
-rd /S /Q gh-pages
-md gh-pages
-md gh-pages\js
-call shadow-cljs release example
-copy resources\public\example\index.html gh-pages
-copy resources\public\example\js\main.js gh-pages\js
-git checkout gh-pages
-copy /Y gh-pages\index.html .
-copy /Y ghpages\js\main.js .\js
