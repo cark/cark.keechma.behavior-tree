c6c6f56e4948cb942090eeab4ed66bdff6e77d3c
cark <carkhy@gmail.com>
2019-07-16 05:50:33 +0200

    Clear it all !

--- kaocha.cmd
+++
@@ -1,1 +0,0 @@
-clojure -A:all-tests %*
--- src/example/cark/keechma/behavior_tree/example/controllers.cljs
+++
@@ -1,6 +0,0 @@
-(ns cark.keechma.behavior-tree.example.controllers
-  (:require [cark.keechma.behavior-tree.example.controllers.initializer :as initializer]))
-
-(defn controllers []
-  (-> {}
-      initializer/register))
