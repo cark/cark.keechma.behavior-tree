260f839b5cfa6e392a68f6a056a68dc3df48f11d
cark <carkhy@gmail.com>
2019-07-16 06:37:43 +0200

    production.bat compiles the exampel and copies it to /gh-pages

--- production.bat
+++ production.bat
@@ -1,1 +1,6 @@
-shadow-cljs release example
+rd /S /Q gh-pages
+md gh-pages
+md gh-pages\js
+call shadow-cljs release example
+copy resources\public\example\index.html gh-pages
+copy resources\public\example\js\main.js gh-pages\js
